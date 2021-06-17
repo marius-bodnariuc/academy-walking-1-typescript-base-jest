40738a6e30912ed0770455bfe707b87326116229
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-06-17 10:48:37 +0100

    mob next [ci-skip]

--- src/test/accountService.test.ts
+++ src/test/accountService.test.ts
@@ -1,4 +1,5 @@
 import {BankAccountService} from "../main/accountService";
+import MockDate from 'mockdate'
 
 describe("Account Service", () => {
   describe("Given the proposed setup in the acceptance test", () => {
@@ -34,4 +35,26 @@ describe("Account Service", () => {
     17/06/2021 || ${amount} || ${amount}
     `);
   });
+
+  it.each(
+    [ '17/06/2021', '18/06/2021']
+  )("dater xxx Prints %i when we deposit %i", (today: string) => {
+    const dateParts = today.split("/").map(e => parseInt(e));
+    const dateObject = new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+    MockDate.set(dateObject);
+
+    const printOutput = jest.fn();
+    const amount = 1000;
+
+    const accountService = new BankAccountService(printOutput);
+
+    accountService.deposit(amount);
+    accountService.printStatement();
+
+    expect(printOutput).toHaveBeenCalledWith(`
+    Date || Amount || Balance
+    ${today} || ${amount} || ${amount}
+    `);
+  });
+
 });
